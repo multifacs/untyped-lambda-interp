760ef4bf082071d2ca9d6136f2396182388054da
unknown <kiwi0tails@gmail.com>
2024-06-01 00:47:24 +0300

    id one two etc

--- src/Language/LambdaCalculus/Parser/Term.hs
+++ src/Language/LambdaCalculus/Parser/Term.hs
@@ -5,7 +5,7 @@ module Language.LambdaCalculus.Parser.Term
   ) where
 
 -- | See http://mattwetmore.me/posts/parsing-combinators-with-parser-combinators.html
-import Data.List (elemIndex)
+import Data.List (elemIndex, isPrefixOf)
 
 import Language.LambdaCalculus.AST
 import Language.LambdaCalculus.Parser.Common
@@ -50,5 +50,24 @@ parseTerm = chainl1 parseNonApp $ do
 parseWith :: Parsec String [u] a -> String -> Either ParseError a
 parseWith p = runParser p [] "untyped lambda-calculus"
 
+replaceLambda :: String -> String
+replaceLambda = go
+  where
+    go [] = []
+    go xs
+      | "id" `isPrefixOf` xs = "\\x.x" ++ go (drop 2 xs)
+      | "tru" `isPrefixOf` xs = "\\t.\\f. t" ++ go (drop 3 xs)
+      | "fls" `isPrefixOf` xs = "\\t.\\f. f" ++ go (drop 3 xs)
+      | "scc" `isPrefixOf` xs = "\\n.\\s.\\z. s (n s z)" ++ go (drop 3 xs)
+      | "plus" `isPrefixOf` xs = "\\m.\\n.\\s.\\z. m s (n s z)" ++ go (drop 4 xs)
+      | "zero" `isPrefixOf` xs = "\\s.\\z. z" ++ go (drop 4 xs)
+      | "one" `isPrefixOf` xs = "\\s.\\z. s z" ++ go (drop 3 xs)
+      | "two" `isPrefixOf` xs = "\\s.\\z. s (s z)" ++ go (drop 3 xs)
+      | "three" `isPrefixOf` xs = "\\s.\\z. s (s (s z))" ++ go (drop 5 xs)
+      | "test" `isPrefixOf` xs = "\\l.\\m.\\.n (l m n)" ++ go (drop 4 xs)
+      | otherwise = head xs : go (tail xs)
+
 parseLC :: String -> Either ParseError Term
-parseLC = parseWith (whiteSpace >> parseTerm)
+parseLC s = do
+  let s1 = replaceLambda s
+  parseWith (whiteSpace >> parseTerm) s1
